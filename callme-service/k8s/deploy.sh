26b963291f462b1ff9c30eae42506695e2ae00c7
Bugakov.Sergey <svbugakov@gmail.com>
2021-10-12 12:06:01 +0300

    refacor

--- callme-service/k8s/deploy.sh
+++ callme-service/k8s/deploy.sh
@@ -8,3 +8,4 @@ docker image push svbugakov/kuber:istio_callme
 kubectl delete deployment callme-app-v1
 kubectl delete deployment callme-app-v2
 kubectl apply -f deployment.yaml
+kubectl apply -f istioDeploy.yaml
